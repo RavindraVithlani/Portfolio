0ff1dfd0563c77159dc8cf7adc247329ea7d73e3
Ravindra <ravindra.vithlani131101@gmail.com>
2024-09-02 21:38:02 +0530

    modified nav bar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,7 +2,8 @@ import { Outlet, Link, useLocation } from "react-router-dom";
 
 export default function Layout() {
     const location = useLocation();
-    const isHomePage = location.pathname === "/";
+    const path = location.pathname
+    const isHomePage = path === "/";
 
     return (
         <div className="container">
@@ -10,11 +11,11 @@ export default function Layout() {
                 <>
                 {/* <div className="logo-img"><img src="/imgs/dp.webp" alt="" /></div> */}
                 <nav className="menu-bar">
-                    <Link to="/" ><p>Home</p> </Link>
-                    <Link to="https://drive.google.com/file/d/1NMrltO50bAfbIrJNYrvqiQcu5xtT4D_y/view?usp=drive_link" target="_blank" rel="noreferrer"><p>Resume</p> </Link>
-                    <Link to="/projects" ><p>Projects</p> </Link>
-                    <Link to="/education"><p>Education</p> </Link>
-                    <Link to="https://drive.google.com/drive/folders/1PrgXrdH0TRdo-6tQoGTOMQFv2d6ovn_D?usp=sharing" target="_blank" rel="noreferrer"><p>Certificates</p> </Link>
+                    <Link className = "nav-links"  to="/" ><p>Home</p> </Link>
+                    <Link className = "nav-links"  to="https://drive.google.com/file/d/1NMrltO50bAfbIrJNYrvqiQcu5xtT4D_y/view?usp=drive_link" target="_blank" rel="noreferrer"><p>Resume</p> </Link>
+                    <Link className = {`nav-links ${path ==="/projects"? "active":""}` }  to="/projects" ><p>Projects</p> </Link>
+                    <Link className = {`nav-links ${path ==="/education"? "active":""}` }  to="/education"><p>Education</p> </Link>
+                    <Link className = "nav-links"  to="https://drive.google.com/drive/folders/1PrgXrdH0TRdo-6tQoGTOMQFv2d6ovn_D?usp=sharing" target="_blank" rel="noreferrer"><p>Certificates</p> </Link>
                 </nav>
                 </>
             )}
